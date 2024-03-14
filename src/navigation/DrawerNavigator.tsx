202121eaff587b51a51f2d913c93dfc65f4788e4
Mateus Cardoso <matecardoso38@gmail.com>
2024-03-14 16:10:10 -0300

    Product list as home screen

--- src/navigation/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator.tsx
@@ -1,34 +1,55 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { useNavigation } from '@react-navigation/native';
 
 import { NavigationContext } from '../contexts/NavigationContext';
 import { configureApiClient } from '../services/ApiService';
+import { logoutUser } from '../services/AuthService';
+
+import { ActivityIndicator, Alert } from 'react-native';
+import { LoadingContainer } from '../styles/DataShowStyles';
 
 import CustomDrawerContent from './CustomDrawerContent';
 
-import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import CompanyScreen from '../screens/CompanyScreen';
 import ConnectionStatusScreen from '../screens/ConnectionStatusScreen';
 import ProductListScreen from '../screens/ProductListScreen';
 
+
 const Drawer = createDrawerNavigator();
 
-
 const DrawerNavigator = () => {
+  const [loading, setLoading] = useState(true);
+
   const { navigationReady } = useContext(NavigationContext);
   const navigation = useNavigation();
 
   useEffect(() => {
     if (navigationReady) {
-      configureApiClient(navigation);
+      configureApiClient(navigation)
+        .then(() => setLoading(false))
+        .catch((error) => {
+          Alert.alert(
+            'Sessão Expirada',
+            'Sua sessão expirou. Por favor, faça o login novamente.',
+            [{ text: 'OK', onPress: () => logoutUser(navigation) }]
+          );
+          setLoading(false);
+        });
     }
   }, [navigation, navigationReady]);
 
+  if (loading) {
+    return (
+      <LoadingContainer>
+        <ActivityIndicator testID="loading-indicator" size="large" color="#007bff" />
+      </LoadingContainer>
+    );
+  }
+
   return (
     <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
-      <Drawer.Screen name="Home" component={HomeScreen} />
       <Drawer.Screen name="Lista de Produtos" component={ProductListScreen} />
       <Drawer.Screen name="Perfil do Usuário" component={ProfileScreen} />
       <Drawer.Screen name="Dados da Empresa" component={CompanyScreen} />
--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -6,37 +6,36 @@ import { getToken } from '../utils/storage';
 import { logoutUser } from './AuthService';
 
 
-export const configureApiClient = (navigation: any) => {
-
-  apiClient.interceptors.request.use(async (config) => {
-    const token = await getToken();
-    if (token) {
-      config.headers['Authorization-Compufacil'] = token;
-      if (config.data && typeof config.data === 'object') {
-        config.data.access_token = token;
-      } else {
-        config.data = { access_token: token };
+export const configureApiClient = async (navigation: any) => {
+  return new Promise(async (resolve, reject) => {
+    apiClient.interceptors.request.use(async (config) => {
+      const token = await getToken();
+      if (token) {
+        config.headers['Authorization-Compufacil'] = token;
+        if (config.data && typeof config.data === 'object') {
+          config.data.access_token = token;
+        } else {
+          config.data = { access_token: token };
+        }
       }
-    }
-    return config;
-  }, (error) => {
-    return Promise.reject(error);
-  });
+      return config;
+    }, (error) => {
+      reject(error);
+      return Promise.reject(error);
+    });
 
-  apiClient.interceptors.response.use(response => response, error => {
-    if (error.response && error.response.data.error_name === 'InvalidToken') {
-      Alert.alert(
-        'Sessão Expirada',
-        'Sua sessão expirou. Por favor, faça o login novamente.',
-        [
-          {
-            text: 'OK',
-            onPress: () => logoutUser(navigation)
-          }
-        ]
-      );
-    }
-    return Promise.reject(error);
+    apiClient.interceptors.response.use(response => response, error => {
+      if (error.response && error.response.data.error_name === 'InvalidToken') {
+        Alert.alert(
+          'Sessão Expirada',
+          'Sua sessão expirou. Por favor, faça o login novamente.',
+          [{ text: 'OK', onPress: () => logoutUser(navigation) }]
+        );
+      }
+      reject(error);
+      return Promise.reject(error);
+    });
+
+    resolve(true);
   });
-  
 };
